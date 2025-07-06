e936480732e6859db74fb6c453bf9752ac7d128a
Pilar Vazquez <pilivaz78@gmail.com>
2025-07-06 16:08:16 -0300

    Corregido archivo page.tsx y ajustadas rutas

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'ProA Corral de Bustos',
+  description: 'Sitio escolar del ProA Corral de Bustos',
 }
 
 export default function RootLayout({
